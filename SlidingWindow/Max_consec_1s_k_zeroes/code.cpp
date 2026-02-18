49dc63a111741ddce0410eb5da9df4e5c89e2323
Ved0705 <158321598+Ved0705@users.noreply.github.com>
2026-02-18 14:51:32 +0530

    Update code.cpp

--- SlidingWindow/Max_consec_1s_k_zeroes/code.cpp
+++ SlidingWindow/Max_consec_1s_k_zeroes/code.cpp
@@ -1,1 +1,20 @@
-
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+      int l=0;
+      int maxlen=0;
+      int zeroes=0;
+      for(int i=0;i<nums.size();i++){
+        if(nums[i]==0){
+          zeroes++;
+    }
+        while(zeroes>k){
+          if(nums[l]==0){
+            zeroes--;
+        }
+          l++;
+}
+maxlen=max(maxlen,i-l+1);
+      }
+return maxlen;
+    };
