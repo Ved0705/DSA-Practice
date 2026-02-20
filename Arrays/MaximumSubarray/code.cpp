ab01e85609d8717ac37e5465903ce75a41035a3e
Ved0705 <158321598+Ved0705@users.noreply.github.com>
2026-02-20 21:53:44 +0530

    Update code.cpp

--- Arrays/MaximumSubarray/code.cpp
+++ Arrays/MaximumSubarray/code.cpp
@@ -1,1 +1,12 @@
-
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+      int currs=nums[0];
+      int maxs=nums[0];
+      for(int i:nums){
+        currs=max(i,i+currs);
+        maxs=max(maxs,currs);
+    }
+      return(maxs);
+}
+}
