43f3821cde9fc95dfa11a40420cc95b16f9fb80d
Ved0705 <158321598+Ved0705@users.noreply.github.com>
2026-02-21 12:30:50 +0530

    Update code.cpp

--- Arrays/3Sum/code.cpp
+++ Arrays/3Sum/code.cpp
@@ -1,1 +1,26 @@
-
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+      vector<vector<int>>result;
+      for(int i=0;i<nums.size()-2;i++){
+        int l=i+1;
+        int r=nums.size()-1;
+        int target= -nums[i];
+        while(l<r){
+          int currsum= nums[l]+nums[r];
+          if(currs==target){
+            result.push_back({nums[i],nums[l],nums[r]});
+            while(l<r && nums[l]==nums[l+1]) l++;
+            while(l<r  && nums[r]==nums[r-1])  r--;
+            l++;
+            r--;
+          }
+          else  if(currs>target){
+            r--;
+          
+          }
+          else{l++;}
+      }
+    }
+      return result;
+}
