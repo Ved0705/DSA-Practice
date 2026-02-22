41dc885cd92399903e98243c4ec70b88f183a1da
Ved0705 <158321598+Ved0705@users.noreply.github.com>
2026-02-22 09:48:34 +0530

    Update code.cpp

--- BinarySearch/FirstLastOccur/code.cpp
+++ BinarySearch/FirstLastOccur/code.cpp
@@ -1,1 +1,46 @@
+class Solution {
+public:
+   int firstoccur(vector<int>&nums, int target){
+    int l=0;
+    int h=nums.size()-1;
+    int ans=-1;
+    while(l<=h){
+        int mid=l+(h-l)/2;
+        if(target==nums[mid]){
+            ans=mid;
+            h=mid-1;
+        }
+        else if(target>nums[mid]){
+            l=mid+1;
+        }
+        else{
+            h=mid-1;
+        }
+       
+    }
+    return ans;
+    }
+   int lastoccur(vector<int>&nums, int target){
+    int l=0;
+    int h=nums.size()-1;
+    int ans=-1;
+    while(l<=h){
+        int mid=l+(h-l)/2;
+        if(target==nums[mid]){
+            ans=mid;
+            l=mid+1;
+        }
+        else if(target>nums[mid]){
+            l=mid+1;
+        }
+        else{
+            h=mid-1;
+        }
+    }
+    return ans;
+   }
 
+    vector<int> searchRange(vector<int>& nums, int target) {
+       return  {firstoccur(nums,target), lastoccur(nums,target)};
+    }
+};
